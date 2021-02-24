cab3490256add7695a44a723ed08b3f443bc2753
Liz Antoinette Anguren <31739093+LizAntoinette@users.noreply.github.com>
2021-02-24 13:04:15 +0800

    Adding Connections Function

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,60 +1,159 @@
 <template>
-<div class="container ">
- <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-  <button type="button" class="btn btn-secondary">Add Vertex</button>
-  <button type="button" class="btn btn-secondary">Add Connection</button>
- 
-  <b-dropdown :text="selectedItem" v-model="selectedItem">
-        <b-dropdown-item @click="selectedItem='BFS'">BFS</b-dropdown-item>
-        <b-dropdown-item @click="selectedItem='DFS'">DFS</b-dropdown-item>
-        <b-dropdown-item @click="selectedItem='Uniform Cost'">Uniform Cost</b-dropdown-item>
-        <b-dropdown-item @click="selectedItem='Greedy BFS'">Greedy BFS</b-dropdown-item>
-        <b-dropdown-item @click="selectedItem='A*'">A*</b-dropdown-item>
-                
-  </b-dropdown>
-  <button type="button" class="btn btn-secondary">Run Graph</button>
-</div>
-  <div class="row pt-5 " >
-     <b-card class="col-12" style="height: 30rem;"  @mouseover="showCoords">
-        <v-stage :config="configKonva">
+  <div class="container">
+    <div
+      class="btn-group"
+      role="group"
+      aria-label="Button group with nested dropdown"
+    >
+      <button type="button" class="btn btn-secondary">Clear</button>
+      <button type="button" class="btn btn-secondary"   @click="addingVertex">Add Vertex</button>
+      <button type="button" class="btn btn-secondary " @click="addingConnection">Add Connection</button>
+
+      <b-dropdown :text="selectedItem" v-model="selectedItem">
+        <b-dropdown-item @click="selectedItem = 'BFS'">BFS</b-dropdown-item>
+        <b-dropdown-item @click="selectedItem = 'DFS'">DFS</b-dropdown-item>
+        <b-dropdown-item @click="selectedItem = 'Uniform Cost'"
+          >Uniform Cost</b-dropdown-item
+        >
+        <b-dropdown-item @click="selectedItem = 'Greedy BFS'"
+          >Greedy BFS</b-dropdown-item
+        >
+        <b-dropdown-item @click="selectedItem = 'A*'">A*</b-dropdown-item>
+      </b-dropdown>
+      <button type="button" class="btn btn-secondary">Run Graph</button>
+    </div>
+    <div class="row pt-5">
+      <b-card class="col-12 drawArea" style="height: 30rem">
+        <!-- <b-card class="col-12 drawArea" style="height: 30rem;"  @mouseover="showCoords" > -->
+        <v-stage :config="stageSize" @click="handleClick" ref="stage" @mousedown="handleMouseDown" @mouseup="handleMouseUp" @mousemove="handleMouseMove">
           <v-layer>
-            <v-circle :config="configCircle"></v-circle>
+            <v-line
+              v-for="line in connections"
+              :key="line.id"
+              :config="{
+                stroke: 'black',
+                points: line.points
+              }"
+            />
+            <v-circle
+              v-for="item in list"
+              :key="item.id"
+              :config="{
+                x: item.x,
+                y: item.y,
+                radius: 10,
+                fill: 'red',
+                //draggable:true,
+                stroke: 'black',
+                strokeWidth: 2
+              }"
+            ></v-circle>
           </v-layer>
         </v-stage>
-     </b-card>
-    
+      </b-card>
+    </div>
   </div>
-</div>
-  
 </template>
 <script>
 export default {
   data() {
     return {
-      selectedItem:'Select Graph',
-      coor:'',
-      configKonva: {
+      list: [],
+      connection: false,
+      vertex: false,
+
+      connections: [],
+      drawningLine: false,
+      selectedItem: "Select Graph",
+      coor: "",
+      stageSize: {
         width: 200,
-        height: 200
+        height: 200,
       },
-      configCircle: {
-        x: 100,
-        y: 100,
-        radius: 70,
-        fill: "red",
-        stroke: "black",
-        strokeWidth: 4
-      }
     };
   },
-  methods:{
-   showCoords(event) {
-      var x = event.clientX;
-      var y = event.clientY;
-      let coor = "X coords: " + x + ", Y coords: " + y; 
-      
-      }
-  }
-};
+  mounted: function () {
+    //For Dynamic Stage Size
+    const container = document.querySelector(".drawArea");
+    const observer = new ResizeObserver(() => {
+      this.stageSize.width = container.offsetWidth;
+      this.stageSize.height = container.offsetHeight;
+    });
+    observer.observe(container);
+  },
+  // updated: function() {
+  //   console.log(this.addingConnection);
+  //   console.log(this.addingVertex);
+  // },
+  methods: {
+    addingConnection(){
+      this.connection = !this.connection;
+      this.vertex = false;
+      console.log(this.connection)
+    },
+    addingVertex(){
+      this.connection = false;
+      this.vertex = !this.vertex;
+      console.log(this.vertex)
+    },
 
+    handleClick(evt) {
+      if(this.vertex){
+        const stage = evt.target.getStage();
+        const pos = stage.getPointerPosition();
+        this.list.push(pos);
+      }
+      //this.save();
+    },
+    handleMouseDown(e) {
+      if(this.connection){
+         const onCircle = e.target instanceof Konva.Circle;
+          if (!onCircle) {
+            return;
+          }
+          this.drawningLine = true;
+          this.connections.push({
+            id: Date.now(),
+            points: [e.target.x(), e.target.y()]
+          });
+      }
+     
+    },
+    handleMouseMove(e) {
+       if(this.connection){
+        if (!this.drawningLine) {
+          return;
+        }
+        const pos = e.target.getStage().getPointerPosition();
+        const lastLine = this.connections[this.connections.length - 1];
+        lastLine.points = [lastLine.points[0], lastLine.points[1], pos.x, pos.y];
+       }
+   },
+    handleMouseUp(e) {
+      if(this.connection){
+        const onCircle = e.target instanceof Konva.Circle;
+        if (!onCircle) {
+          return;
+        }
+        this.drawningLine = false;
+        const lastLine = this.connections[this.connections.length - 1];
+        lastLine.points = [
+          lastLine.points[0],
+          lastLine.points[1],
+          e.target.x(),
+          e.target.y()
+        ];
+      }
+    },
+    clearScreen() {
+      this.list = [];
+    },
+    // showCoords(event) {
+    //   this.coor_x = event.clientX;
+    //   this.coor_y = event.clientY;
+    //   let coor = "X coords: " + this.coor_x + ", Y coords: " + this.coor_y;
+
+    // }
+  },
+};
 </script>
