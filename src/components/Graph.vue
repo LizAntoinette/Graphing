9cd81659c520a7d342c3eee39793f008ef346186
Liz Antoinette Anguren <31739093+LizAntoinette@users.noreply.github.com>
2021-03-02 13:27:46 +0800

    Adding Notes for the Algorithms

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -26,7 +26,7 @@
         >
         <b-dropdown-item @click="selectedItem = 'A*'">A*</b-dropdown-item>
       </b-dropdown>
-      <button type="button" class="btn btn-secondary">Run Graph</button>
+      <button type="button" class="btn btn-secondary" @click="runGraph">Run Graph</button>
     </div>
     <div class="row pt-5">
       <b-card class="col-12 drawArea" style="height: 30rem">
@@ -55,6 +55,7 @@
               :config="{
                 x: item.x,
                 y: item.y,
+                
                 radius: 10,
                 fill: 'red',
                 //draggable:true,
@@ -62,6 +63,17 @@
                 strokeWidth: 2,
               }"
             ></v-circle>
+            <v-text
+              v-for="(item, index) in list"
+              :key="item.id"
+              :config="{
+                x: item.x + 6,
+                y: item.y + 6,
+                text: index,
+                fontSize: 15,
+                width: 300,
+              }" 
+            ></v-text>
             <v-text
               v-for="label in distances"
               :key="label.id"
@@ -81,6 +93,7 @@
 export default {
   data() {
     return {
+   
       list: [],
       edges: [],
       distances: [],
@@ -89,6 +102,7 @@ export default {
 
       connections: [],
       drawningLine: false,
+      runnableGraph: false,
       selectedItem: "Select Graph",
       coor: "",
       stageSize: {
@@ -123,10 +137,11 @@ export default {
     },
 
     handleClick(evt) {
-      if (this.vertex) {
+      if (this.vertex && this.list.length < 20) {
         const stage = evt.target.getStage();
         const pos = stage.getPointerPosition();
         this.list.push(pos);
+      
       }
       //this.save();
     },
@@ -190,6 +205,8 @@ export default {
             Math.min(e.target.y(), lastLine.points[1]) +
             Math.abs(e.target.y() - lastLine.points[1]) / 2 +
             4,
+          point1: indexOfPoint1,
+          point2: indexOfPoint2
         });
         console.log(this.distances);
         //    this.edges[indexOfPoint1][indexOfPoint2] = dist.toFixed(2);
@@ -202,12 +219,40 @@ export default {
         ];
         console.log(this.connections);
         console.log(this.list);
+        if(this.connections.length < 2){
+            this.runnableGraph = true
+        }
       }
     },
     clearScreen() {
       this.list = [];
       this.connections = [];
       this.distances = [];
+    },
+    createGrid(){
+      const size = this.list.length;
+      let grid = new Array(size);
+
+      //initializing the grid
+      for (let i = 0; i < size; i++) {
+          grid[i] = new Array(size);
+          
+      }
+      
+      //setting the distances in the grid
+      for (let j = 0; j < this.distances.length; j++) {
+         let x = this.distances[j].point1;
+         let y = this.distances[j].point2;
+         grid[x][y] = this.distances[j].distance;
+         grid[y][x] = this.distances[j].distance;
+      }
+       console.log(grid);
+
+    },
+    runGraph(){
+       if(this.runnableGraph){
+          this.createGrid();
+       }
     },
     aStarMethod() {},
     greedyBFS() {},
