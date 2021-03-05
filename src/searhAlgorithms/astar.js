4bafaf8c2ab2204d316aa95ec1f27adc5fc4ed29
Liz Antoinette Anguren <31739093+LizAntoinette@users.noreply.github.com>
2021-03-05 22:32:08 +0800

    adding astar nope, I do not understand what I'm doing haha

--- src/searhAlgorithms/astar.js
+++ src/searhAlgorithms/astar.js
@@ -1,5 +1,42 @@
-export function astar(startNode, endNode, graph, heuristic, size) {}
+import PriorityQueue from "js-priority-queue";
 
+export function astar(startNode, endNode, graph, heuristic, size) {
+  let visited = Array(size).fill(false);
+  let pq = new PriorityQueue(); //states??
+  let actions = [];
+  pq.enqueue((startNode, []), heuristic(startNode));
+  let nCost = 0;
+  while (pq.length !== 0) {
+    let node = pq.dequeue();
+    actions.push(node);
+    if (node === endNode) {
+      return actions;
+    }
+    if (!visited[node]) {
+      let nb = getNeighboors(graph[node]);
+      for (let j = 0; j < nb.length; j++) {
+        if (!visited[j]) {
+          let distToNode = graph[node][j];
+          let distances = node[j] + distToNode;
+          nCost = distances + heuristic[j];
+          actions.push((nb[j], distances), nCost);
+        }
+      }
+    }
+    visited[node] = true;
+  }
+  return actions;
+}
+
+function getNeighboors(arr) {
+  let neighboors = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      neighboors.push(i);
+    }
+  }
+  return neighboors;
+}
 // def aStarSearch(problem, heuristic=nullHeuristic):
 //     "Search the node that has the lowest combined cost and heuristic first."
 //     start = problem.getStartState()
