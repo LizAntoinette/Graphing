86e254fe7e690d602f267831f244389fbf9f5c78
Liz Antoinette Anguren <31739093+LizAntoinette@users.noreply.github.com>
2021-03-05 16:13:11 +0800

    Initializing Astar algorithm

--- src/searhAlgorithms/astar.js
+++ src/searhAlgorithms/astar.js
@@ -1,128 +1,25 @@
-export function astar(grid = [], startNode, finishNode) {
-  const closedlist = [];
-  const openlist = [];
+export function astar(startNode, endNode, graph, heuristic, size) {}
 
-  startNode.cost = {
-      F: 0,
-      G: 0,
-      H: 0,
-  };
-
-  openlist.push(startNode);
-
-  while (!!openlist.length) {
-      openlist.sort((a, b) => a.cost.F - b.cost.F);
-      const current = openlist.shift();
-
-      closedlist.push(current);
-
-      if (current.isWall) continue;
-
-      if (current === finishNode)
-          return [closedlist, calculatePath(finishNode)];
-
-      const neighbors = getNeighbors(grid, current);
-
-      for (let i = 0; i < neighbors.length; i++) {
-          const nNode = neighbors[i];
-          nNode.isVisited = true;
-          if (closedlist.includes(nNode)) continue;
-
-          nNode.cost.G = calculateCost(nNode, startNode, "E");
-          nNode.cost.H = calculateCost(nNode, finishNode, "E");
-          nNode.cost.F = nNode.cost.G + nNode.cost.H;
-
-          if (!openlist.includes(nNode)) {
-              nNode.previousNode = current;
-              openlist.push(nNode);
-          }
-      }
-  }
-  return [closedlist, calculatePath(finishNode)];
-}
-
-function calculateCost(currentNode, node, distanceType) {
-  switch (distanceType) {
-      // Euclidean Distance
-      case "E":
-          return Math.floor(
-              Math.sqrt(
-                  Math.pow(currentNode.row - node.row, 2) +
-                      Math.pow(currentNode.col - node.col, 2)
-              ) * 10
-          );
-
-      // Manhattan Distance
-      case "M":
-          return (
-              Math.abs(currentNode.row - node.row) +
-              Math.abs(currentNode.col - node.col)
-          );
-
-      // Diagonal Distance
-      case "D":
-          return Math.max(
-              Math.abs(currentNode.row - node.row),
-              Math.abs(currentNode.col - node.col)
-          );
-      default:
-          return 0;
-  }
-}
-
-function getNeighbors(grid = [], currentNode) {
-  const ROWS = grid.length;
-  const COLS = grid[0].length;
-  const { row, col } = currentNode;
-  const neighbors = [];
-
-  // Top, Left, Right, Bottom Nodes
-  if (
-      row + 1 >= 0 &&
-      row + 1 < ROWS &&
-      col >= 0 &&
-      col < COLS &&
-      !grid[row + 1][col].isWall
-  ) {
-      neighbors.push(grid[row + 1][col]);
-  }
-  if (
-      row - 1 >= 0 &&
-      row - 1 < ROWS &&
-      col >= 0 &&
-      col < COLS &&
-      !grid[row - 1][col].isWall
-  ) {
-      neighbors.push(grid[row - 1][col]);
-  }
-  if (
-      row >= 0 &&
-      row < ROWS &&
-      col - 1 >= 0 &&
-      col - 1 < COLS &&
-      !grid[row][col - 1].isWall
-  ) {
-      neighbors.push(grid[row][col - 1]);
-  }
-  if (
-      row >= 0 &&
-      row < ROWS &&
-      col + 1 >= 0 &&
-      col + 1 < COLS &&
-      !grid[row][col + 1].isWall
-  ) {
-      neighbors.push(grid[row][col + 1]);
-  }
-
-  return neighbors;
-}
-
-function calculatePath(finishNode) {
-  const shortestPathNodes = [];
-  let currentNode = finishNode;
-  while (currentNode !== null) {
-      shortestPathNodes.unshift(currentNode);
-      currentNode = currentNode.previousNode;
-  }
-  return shortestPathNodes;
-}
+// def aStarSearch(problem, heuristic=nullHeuristic):
+//     "Search the node that has the lowest combined cost and heuristic first."
+//     start = problem.getStartState()
+//     exploredState = []
+//     states = util.PriorityQueue()
+//     states.push((start, []), nullHeuristic(start, problem))
+//     nCost = 0
+//     while not states.isEmpty():
+//         state, actions = states.pop()
+//         if problem.isGoalState(state):
+//             return actions
+//         if state not in exploredState:
+//             successors = problem.getSuccessors(state)
+//             for succ in successors:
+//                 coordinates = succ[0]
+//                 if coordinates not in exploredState:
+//                     directions = succ[1]
+//                     nActions = actions + [directions]
+//                     nCost = problem.getCostOfActions(nActions) + heuristic(coordinates, problem)
+//                     states.push((coordinates, actions + [directions]), nCost)
+//         exploredState.append(state)
+//     return actions
+//     util.raiseNotDefined()
