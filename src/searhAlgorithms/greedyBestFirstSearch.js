19be1b22d15b2b3d617d6bd2a4a8eddb041bb597
Liz Antoinette Anguren <31739093+LizAntoinette@users.noreply.github.com>
2021-03-05 15:54:14 +0800

    adding initial greedy bfs

---
+++ src/searhAlgorithms/greedyBestFirstSearch.js
@@ -0,0 +1,46 @@
+import PriorityQueue from 'js-priority-queue'
+
+export function greedyBestFirstSearch(startNode, endNode, graph, heuristic, size){
+  let visited = Array(size).fill(false);
+  visited[startNode] = true;
+  let pq = new PriorityQueue();
+  pq.queue((0, startNode));
+  let path = [];
+  while(pq.length !== 0){
+      var node = pq.dequeue();
+      path.push(node);
+      if(node === endNode){
+        break;
+      }
+
+      for(let i = 0; i< size; i++){
+        if(!visited[i]){
+          visited[i] = true;
+          pq.enqueue((graph[i], i));
+        }
+      }
+
+
+  }
+  return path;
+
+}
+
+// def best_first_search(source, target, n):
+//     visited = [0] * n
+//     visited = True
+//     pq = PriorityQueue()
+//     pq.put((0, source))
+//     while pq.empty() == False:
+//         u = pq.get()[1]
+//         # Displaying the path having lowest cost
+//         print(u, end=" ")
+//         if u == target:
+//             break
+ 
+//         for v, c in graph[u]:
+//             if visited[v] == False:
+//                 visited[v] = True
+//                 pq.put((c, v))
+//     print()
+ 
